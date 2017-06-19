cc68511786a6499599a21cf48736edc800edb9bc
liurenyi <928171620@qq.com>
2017-06-19 14:34:59 +0800

    preference 相关使用 -- 资源文件

---
+++ app/src/main/java/com/android/remoteview/BlankFragment.java
@@ -0,0 +1,30 @@
+package com.android.remoteview;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends PreferenceFragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
